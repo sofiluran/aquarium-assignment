490e266c6f51b3db16862c1c1753cbd9d2bb3691
Seth <sdarmistead@gmail.com>
2026-02-09 18:02:36 +0100

    removed comments, cleaned up formatting

--- routes/octopi.js
+++ routes/octopi.js
@@ -5,24 +5,22 @@ import { octopi } from '../data/creatures.js'
 const octopiRouter = express.Router()
 const categoryName = "Octopi"
 
-//Returns an array with all the species in a category//
 const getSidebarContent = () => {
   let sideBarData = octopi.map(e => e.name)
   return sideBarData
 }
 
-//Returns an object with the species data//
 const findSpecies = (speciesName) => {
   let formattedName = speciesName.replace(/-/g, ' ')
   let speciesObject = octopi.find(e => e.name.toLowerCase() === formattedName)
   return speciesObject
 }
 
-//Returns an object with the species group info//
 const findCategory = () => {
   let currentCategory = groups.find(e => e.name === categoryName)
   return currentCategory
 }
+
 
 octopiRouter.get('/', (req, res) => {
   res.render('pages/index', {
