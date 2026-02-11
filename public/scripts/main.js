c51dac1b595071e13486710e24bfa1ddfab8b8f6
Seth Armistead <sdarmistead@gmail.com>
2026-02-11 18:02:10 +0100

    added animations, styled more. added fade stuff and more

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,88 @@
-const slideIn = (element) => {
+const slideInLeft = (element) => {
   gsap.from(element, {
-    x: -400,
-    
+    x: -100,
+    duration: .7,
+    ease: "power1.out",
+    stagger: .03
   })
 }
+
+const slideInRight = (element) => {
+  gsap.from(element, {
+    x: 100,
+    duration: .9,
+    ease: "elastic",
+    stagger: .03
+  })
+}
+
+const slideUp = (element) => {
+  gsap.from(element, {
+    y: 80,
+    duration: .6,
+    ease: "power1.out",
+    stagger: .09
+  })
+}
+
+const fadeIn = (element) => {
+  gsap.from(element, {
+    opacity: 0,
+    ease: "power2out",
+    duration: 1.1
+  })
+}
+
+const dropDown = (element) => {
+  gsap.from(element, {
+    y: -50,
+    duration: .6,
+    ease: "bounce",
+    stagger: .03
+  })
+}
+
+const expandLine = (element) => {
+  gsap.to(element, {
+    scaleX: 1,
+    duration: .4,
+    ease: "back.out(1.2)",
+  })
+}
+
+
+const shrinkLine = (element) => {
+  gsap.to(element, {
+    scaleX: 0,
+    duration: .6,
+    ease: "power1.out",
+  })
+}
+
+const navItems = document.querySelectorAll('.nav-item')
+navItems.forEach(item => {
+  let underlinedItem = item.querySelector('.nav-underline')
+  item.addEventListener('mouseenter', ()=> expandLine(underlinedItem))
+  item.addEventListener('mouseleave', ()=> shrinkLine(underlinedItem))
+});
+
+const leftItems = document.querySelectorAll('.nav-left .nav-item')
+const rightItems = document.querySelectorAll('.nav-right .nav-item')
+const title = document.querySelector('.title')
+const sidebarItems = document.querySelectorAll('.sidebar-item')
+const sidebarHeader = document.querySelector('.sidebar-header')
+const logo = document.querySelector('.logo')
+
+
+
+fadeIn(logo)
+dropDown(logo)
+// slideInLeft(leftItems)
+// slideInRight(rightItems)
+dropDown(navItems)
+fadeIn(navItems)
+fadeIn(title)
+slideUp(title)
+slideInLeft(sidebarHeader)
+fadeIn(sidebarItems)
+slideUp(sidebarItems)
